989f4c05e3d3a4b9a9d80152dcf8ad7c1bddde44
irembuseozkose <irembuseozkose@gmail.com>
2025-03-10 01:03:25 +0300

    Anasayfa eklendi ve layout düzenlendi.

---
+++ Pages/sanatcilar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SanalSanatGalerisi.Pages
+{
+    public class sanatcilarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
