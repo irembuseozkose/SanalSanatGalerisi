a5298d302cd0e6029a6e36e61a99e7409387ba61
irembuseozkose <irembuseozkose@gmail.com>
2025-03-10 02:14:36 +0300

    Sanatlar eklendi. Sanatçılar düzenlendi.

---
+++ Pages/Akimlarcshtml.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SanalSanatGalerisi.Pages
+{
+    public class AkimlarcshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Sanatlar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SanalSanatGalerisi.Pages
+{
+    public class SanatlarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
